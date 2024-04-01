9aafe5bd3be3c0c04de5e4e5b83a6f9e17b99ee6
camosss <camosss@naver.com>
2024-04-02 03:03:23 +0900

    delete: AppInfo, 사용하지 않는 프로퍼티 제거

--- AppStore/Domain/Model/Response/AppInfo.swift
+++ AppStore/Domain/Model/Response/AppInfo.swift
@@ -10,33 +10,16 @@ import Foundation
 struct AppInfo: Codable {
     let artistId: Int?
     let artistName: String?
-    let artistViewUrl: String?
     let artworkUrl512: String?
-    let sellerName: String?
-    let sellerUrl: String?
     let trackId: Int?
     let trackName: String?
-    let trackCensoredName: String?
     let trackViewUrl: String?
-    let trackContentRating: String?
+    let sellerName: String?
     let description: String?
-    let contentAdvisoryRating: String?
     let averageUserRating: Double?
     let userRatingCount: Int?
     let screenshotUrls: [String]?
-    let version: String?
-    let minimumOsVersion: String?
-    let currentVersionReleaseDate: String?
     let releaseNotes: String?
-    let releaseDate: String?
-    let genreIds: [String]?
-    let genres: [String]?
-    let primaryGenreId: Int?
-    let primaryGenreName: String?
-    let bundleID: String?
-    let isVppDeviceBasedLicensingEnabled: Bool
-    let languageCodesISO2A: [String]?
-    let formattedPrice: String?
 }
 
 extension AppInfo: Equatable {
